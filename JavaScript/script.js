bb806dd136f811a345cea42806e9017cb1ff11f3
shubhankarsingh030 <113350090+shubhankarsingh030@users.noreply.github.com>
2023-10-04 17:53:25 +0530

    Alert And Prompt

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -95,3 +95,33 @@ if (str[0] === "a" && str.length > 3){
 }else{
     console.log("Not a Good String");
 }
+//print Day of the Week
+
+let day = 1;
+
+switch(day){
+    case 1:
+        console.log("Monday")
+        break;
+    case 2:
+        console.log("Tuesday")
+        break;
+    case 3:
+        console.log("Wednesday")
+        break;
+    case 4:
+        console.log("Thursday")
+        break;
+    case 5:
+        console.log("Friday")
+        break;
+    case 6:
+        console.log("Saturday")
+        break;
+    case 7:
+        console.log("Sunday")
+        break;
+    default:
+        console.log("Wrong Day!")
+
+}
---
+++ JavaScript/script2.js
@@ -0,0 +1,6 @@
+alert("Hello Guys");
+
+let firstName = prompt("Enter your First Name ");
+let lastName = prompt("Enter Your Last Name ");
+let msg = "Welcome " + firstName + " " + lastName + "!";
+alert(msg)
