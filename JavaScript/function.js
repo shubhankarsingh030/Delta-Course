0e0144e3e6d606adc90170daa536c86ae77d5e43
shubhankarsingh030 <113350090+shubhankarsingh030@users.noreply.github.com>
2023-10-12 15:45:53 +0530

    Function

---
+++ JavaScript/function.js
@@ -0,0 +1,17 @@
+// function isAdult(){
+//     let age = 19;
+
+//     if (age>=18){
+//         console.log("Adult");
+//     }else{
+//         console.log("Not Adult")
+//     }
+// }
+// isAdult();
+
+function rollDice(){
+    let random = Math.floor(Math.random()*6)+1;
+    console.log(random);
+}
+
+rollDice();
