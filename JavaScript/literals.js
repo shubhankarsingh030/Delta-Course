a966b7c83cd8afcddbf90a41a31d5f8a3a8b750b
shubhankarsingh030 <113350090+shubhankarsingh030@users.noreply.github.com>
2023-10-12 15:45:35 +0530

    Objects Literals

---
+++ JavaScript/literals.js
@@ -0,0 +1,13 @@
+let students = {
+    name: "Shubhanka singh",
+    age: 21,
+    marks: 8.0,
+    city : "Lucknow"
+
+};
+
+let car ={
+    name: "Lamborghini",
+    model: "V6",
+    color: "red"
+};
